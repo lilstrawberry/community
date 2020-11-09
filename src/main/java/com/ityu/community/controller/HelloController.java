2800eba15c80cca06e872770ecc7752d979eb1f9
yuge <1845232215@qq.com>
2020-11-09 20:22:51 +0800

    add index and bootstrap

--- src/main/java/com/ityu/community/controller/HelloController.java
+++ src/main/java/com/ityu/community/controller/HelloController.java
@@ -11,12 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name") String name, Model model) {
-
-        model.addAttribute("name", name);
-
-        return "myhello";
+    @GetMapping("/")
+    public String hello() {
+        return "index";
     }
 
 
